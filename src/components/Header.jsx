16bdde93c8e22284647f4dc07b9ba33c93e47340
seongjaehong <tjdwo0423@gmail.com>
2025-06-08 03:57:15 +0900

    add class

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import './css/Header.css';
+
 export default function Header() {
   return (
     <header className='flex justify-between items-center mb-4'>
-      <h1 className='text-xl font-bold'>독일 생활 표현 사전</h1>
+      <h1 className='title'>
+        <a href='/'>독일 생활 표현 사전</a>
+      </h1>
       <div>
         <button onClick={() => setPage('favorites')}>즐겨찾기</button>
       </div>
