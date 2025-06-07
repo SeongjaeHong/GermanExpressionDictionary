2f8cb5ec933610a4318520b1fff1d0bed83cb31b
seongjaehong <tjdwo0423@gmail.com>
2025-06-08 03:57:26 +0900

    bug fix: splice -> slice

--- src/scripts/csvConvertor.js
+++ src/scripts/csvConvertor.js
@@ -5,7 +5,7 @@ const lines = text.split('\r\n');
 
 export async function fetchData() {
   const data = {};
-  for (const line of lines.splice(1)) {
+  for (const line of lines.slice(1)) {
     if (!line) continue;
     const tokens = line.split(',');
     if (!(tokens[1] in data)) {
@@ -25,13 +25,11 @@ export async function fetchPartData(index = 0) {
   const data = {};
   const unit = 5;
   const start = index * unit + 1;
-  const end = start + unit > lines.length - 1 ? lines.length - 1 : start + unit;
-
+  const end = start + unit > lines.length ? lines.length : start + unit;
   if (start >= end) {
     return null;
   }
-
-  for (const line of lines.splice(start, end)) {
+  for (const line of lines.slice(start, end)) {
     if (!line) continue;
     const tokens = line.split(',');
     if (!(tokens[1] in data)) {
