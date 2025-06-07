07c5786e42259cbcd8778ecfbfc65e7534d87e33
seongjaehong <tjdwo0423@gmail.com>
2025-06-08 01:33:03 +0900

    active card loading

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,42 @@
 import PhraseCard from './PhraseCard.jsx';
-import { fetchData } from '../scripts/csvConvertor.js';
+import { fetchData, fetchPartData } from '../scripts/csvConvertor.js';
 import './css/Main.css';
-
-let data = await fetchData('data/sample.csv');
+import { useEffect, useRef, useState } from 'react';
 
 export default function Main() {
+  const [data, setData] = useState({});
+  const dataRef = useRef(0);
+  const dataHandler = async () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      let loadedData = await fetchPartData(dataRef.current);
+      if (!loadedData) {
+        return;
+      }
+
+      let freshData = Object.assign({}, data);
+      Object.entries(loadedData).forEach(([category, contents]) => {
+        if (!freshData[category]) {
+          freshData[category] = [];
+        }
+        freshData[category].push(...contents);
+      });
+      setData(freshData);
+      dataRef.current += 1;
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      dataHandler();
+    }
+
+    window.addEventListener('scroll', dataHandler);
+    console.log(window.innerHeight, window.scrollY, document.body.offsetHeight);
+    return () => {
+      window.removeEventListener('scroll', dataHandler);
+    };
+  }, [data]);
+
   return (
     <main>
       {Object.entries(data).map(([category, exps]) =>
--- src/scripts/csvConvertor.js
+++ src/scripts/csvConvertor.js
@@ -1,11 +1,37 @@
-export async function fetchData(csv_path) {
-  const text = await fetch(csv_path)
-    .then((res) => res.text())
-    .catch((err) => console.error(err));
+const text = await fetch('data/sample.csv')
+  .then((res) => res.text())
+  .catch((err) => console.error(err));
+const lines = text.split('\r\n');
 
+export async function fetchData() {
   const data = {};
-  const lines = text.split('\r\n');
   for (const line of lines.splice(1)) {
+    if (!line) continue;
+    const tokens = line.split(',');
+    if (!(tokens[1] in data)) {
+      data[tokens[1]] = [];
+    }
+    data[tokens[1]].push({
+      id: tokens[0],
+      German: tokens[2],
+      Korean: tokens[3],
+    });
+  }
+
+  return data;
+}
+
+export async function fetchPartData(index = 0) {
+  const data = {};
+  const unit = 5;
+  const start = index * unit + 1;
+  const end = start + unit > lines.length - 1 ? lines.length - 1 : start + unit;
+
+  if (start >= end) {
+    return null;
+  }
+
+  for (const line of lines.splice(start, end)) {
     if (!line) continue;
     const tokens = line.split(',');
     if (!(tokens[1] in data)) {
