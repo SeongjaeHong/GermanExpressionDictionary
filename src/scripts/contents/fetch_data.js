d01a57c78f92809f3eb64012d25c9e0fc6a8afab
seongjaehong <tjdwo0423@gmail.com>
2025-06-09 00:04:46 +0900

    fetch starred contents

--- src/scripts/contents/fetch_data.js
+++ src/scripts/contents/fetch_data.js
@@ -1,9 +1,14 @@
+import { ROUTES, STARRED } from '../../assets/constants';
+
 const text = await fetch('data/sample.csv')
   .then((res) => res.text())
   .catch((err) => console.error(err));
-const lines = text.split('\r\n');
+const lines = text
+  .split('\r\n')
+  .filter((line) => line !== '')
+  .slice(1); // first low is a column
 
-export async function fetchData() {
+export function fetchData() {
   const data = {};
   for (const line of lines.slice(1)) {
     if (!line) continue;
@@ -21,16 +26,30 @@ export async function fetchData() {
   return data;
 }
 
-export async function fetchPartData(index = 0) {
+export const fetchFunctionHouse = {
+  [ROUTES.home]: fetchPartData,
+  [ROUTES.starred]: fetchStarredData,
+};
+
+export function fetchPartData(index = 0, ids = null) {
   const data = {};
+
+  let fetchedLines = [];
+  if (ids instanceof Array) {
+    fetchedLines = ids.map((id) => lines[id - 1]);
+  } else {
+    fetchedLines = lines;
+  }
+
   const unit = 5;
-  const start = index * unit + 1;
-  const end = start + unit > lines.length ? lines.length : start + unit;
+  const start = index * unit;
+  const end =
+    start + unit > fetchedLines.length ? fetchedLines.length : start + unit;
   if (start >= end) {
     return null;
   }
-  for (const line of lines.slice(start, end)) {
-    if (!line) continue;
+
+  for (const line of fetchedLines.slice(start, end)) {
     const tokens = line.split(',');
     if (!(tokens[1] in data)) {
       data[tokens[1]] = [];
@@ -45,13 +64,19 @@ export async function fetchPartData(index = 0) {
   return data;
 }
 
-export const STARRED = Symbol('starred');
-export async function fetchStarredData(index = 0) {
-  const data = {};
-  const unit = 5;
-  const start = index * unit + 1;
-  const end = start + unit > lines.length ? lines.length : start + unit;
-  if (start >= end) {
-    return null;
+export function fetchStarredData(index = 0) {
+  const starredIds = JSON.parse(localStorage.getItem(STARRED));
+  if (starredIds instanceof Array && !starredIds.length) return null;
+  else if (!starredIds) return null;
+
+  return fetchPartData(index, starredIds);
+}
+
+export function getStarredIds() {
+  let starredIds = JSON.parse(localStorage.getItem(STARRED));
+  if (!starredIds) {
+    starredIds = [];
   }
+
+  return starredIds;
 }
