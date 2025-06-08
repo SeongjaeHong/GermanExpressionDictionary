11ff2979a32443b1e920d39bfbdfed307ce10be2
seongjaehong <tjdwo0423@gmail.com>
2025-06-09 00:05:38 +0900

    change a tag to Link tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
+import { Link } from 'react-router';
 import './css/Header.css';
+import { ROUTES } from '../assets/constants';
 
 export default function Header() {
   return (
     <header className='flex justify-between items-center mb-4'>
       <h1 className='title'>
-        <a href='/'>독일 생활 표현 사전</a>
+        <Link to={ROUTES.home}>독일 생활 표현 사전</Link>
       </h1>
       <div>
-        <button onClick={() => setPage('favorites')}>즐겨찾기</button>
+        <Link to={ROUTES.starred}>즐겨찾기</Link>
       </div>
     </header>
   );
