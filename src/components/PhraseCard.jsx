6f9779a308a0b60cac685cfb7aca753a87107d9a
seongjaehong <tjdwo0423@gmail.com>
2025-06-08 20:02:06 +0900

    add star icon

--- src/components/PhraseCard.jsx
+++ src/components/PhraseCard.jsx
@@ -1,12 +1,23 @@
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar as coloredFaStar } from '@fortawesome/free-solid-svg-icons';
 import './css/PhraseCard.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useState } from 'react';
 export default function PhraseCard({ category, german, korean }) {
+  const [starred, setStar] = useState(false);
+  const starHandler = () => setStar(!starred);
   return (
     <div className='phraseCard'>
       <div className='contents'>
         <p className='german'>{german}</p>
         <p className='translation'>{korean}</p>
       </div>
-      <div className='category'>{category}</div>
+      <div className='rightPannel'>
+        <div className='category'>{category}</div>
+        <div className='star' onClick={starHandler}>
+          <FontAwesomeIcon icon={starred ? coloredFaStar : faStar} />
+        </div>
+      </div>
     </div>
   );
 }
